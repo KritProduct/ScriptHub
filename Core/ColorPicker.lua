dfb625a502a4dbc92ca18b98687e23024160e1fd
KritProduct <kritproduct@gmail.com>
2026-08-17 12:59:52 +0400

    White top, black bottom

--- Core/ColorPicker.lua
+++ Core/ColorPicker.lua
@@ -54,13 +54,29 @@ function ColorPicker.Create(parent, callback, defaultColor)
     canvasCorner.CornerRadius = UDim.new(0, 6)
     canvasCorner.Parent = colorCanvas
     
-    local brightnessGradient = Instance.new("UIGradient")
-    brightnessGradient.Transparency = NumberSequence.new({
+    local whiteGradient = Instance.new("UIGradient")
+    whiteGradient.Color = ColorSequence.new({
+        ColorSequenceKeypoint.new(0, Color3.new(1, 1, 1)),
+        ColorSequenceKeypoint.new(1, Color3.new(1, 1, 1))
+    })
+    whiteGradient.Transparency = NumberSequence.new({
         NumberSequenceKeypoint.new(0, 0),
         NumberSequenceKeypoint.new(1, 1)
     })
-    brightnessGradient.Rotation = 270
-    brightnessGradient.Parent = colorCanvas
+    whiteGradient.Rotation = 90
+    whiteGradient.Parent = colorCanvas
+    
+    local blackGradient = Instance.new("UIGradient")
+    blackGradient.Color = ColorSequence.new({
+        ColorSequenceKeypoint.new(0, Color3.new(0, 0, 0)),
+        ColorSequenceKeypoint.new(1, Color3.new(0, 0, 0))
+    })
+    blackGradient.Transparency = NumberSequence.new({
+        NumberSequenceKeypoint.new(0, 1),
+        NumberSequenceKeypoint.new(1, 0)
+    })
+    blackGradient.Rotation = 90
+    blackGradient.Parent = colorCanvas
     
     local canvasIndicator = Instance.new("Frame")
     canvasIndicator.Size = UDim2.new(0, 10, 0, 10)
